64d6de0b647155ef3a1bc1c21568b8bff029d921
galickiy <nightnomed@gmail.com>
2017-05-11 16:13:14 +0300

    исправлено задание №24

--- 24.php
+++ 24.php
@@ -7,9 +7,13 @@
  */
 
 $ch = 442158755745;
+$num = 4;
+$result = 0;
 settype($ch,'string');
-$sum=0;
-for ($i=0;$i<=strlen($ch)-1;$i++){
-    $sum+=(int)$ch[$i];
+settype($num,'string');
+for($i=0;$i<=strlen($ch)-1;$i++){
+    if($ch[$i] == $num){
+        $result++;
+    }
 }
-echo $sum;
+echo 'Цифра '.$num.' в числе '.$ch.' встречается '.$result.' раза.';
