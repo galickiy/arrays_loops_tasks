25c5ecb1de1ca1b365534221a32c719944b6341b
galickiy <nightnomed@gmail.com>
2017-05-11 14:39:37 +0300

    исправлено задание №2

--- 2.php
+++ 2.php
@@ -7,7 +7,7 @@
 $arr = array(1, 20 ,15, 17 , 24, 35);
 $result = 0;
 foreach ($arr as $item){
-    $result = $result + item; // $result += $result;
+    $result = $result + $item; // $result += $result;
 
 }
  echo $result;
